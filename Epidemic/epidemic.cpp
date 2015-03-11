18053b1130ba29e203577a64e12f3623edd960d6
shchaurouski.slava <shchaurouski.slava@gmail.com>
2015-03-11 20:48:28 +0300

    added file reading

--- Epidemic/epidemic.cpp
+++ Epidemic/epidemic.cpp
@@ -29,8 +29,27 @@ int main() {
 	ifstream fin("input.txt");
 	ofstream fout("output.txt");
 
+	int A = 0; //number of patients A
+	int B = 0; //number of patients B
+	int P = 0; //number of hospital wards
+	int n = 0; //number of beds
+	int a = 0; //number of patients A in a hospital ward
+	int b = 0; //number of patients B in a hospital ward
 
+	fin >> A;
+	fin >> B;
+	fin >> P;
 
+	HospitalWard *wards = new HospitalWard[P];
+
+	for (int i = 0; i < P; i++) {
+
+		fin >> n;
+		fin >> a;
+		fin >> b;
+
+		wards[i] = HospitalWard(n, a, b);
+	}
 
 	return 0;
 }
