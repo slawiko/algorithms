e579a3cfe394983051bc1d223d84bd9b472962b6
shchaurouski.slava <shchaurouski.slava@gmail.com>
2015-03-11 20:32:46 +0300

    struct HospitalWard added

--- Epidemic/epidemic.cpp
+++ Epidemic/epidemic.cpp
@@ -2,8 +2,32 @@
 
 using namespace std;
 
+struct HospitalWard{
+
+	int beds;
+	int patientsA;
+	int patientsB;
+
+	HospitalWard() {
+
+		this->beds = 0;
+		this->patientsA = 0;
+		this->patientsB = 0;
+	}
+
+	HospitalWard(int beds, int patientsA, int patientsB) {
+
+		this->beds = beds;
+		this->patientsA = patientsA;
+		this->patientsB = patientsB;
+	}
+};
+
+
 int main() {
 
+	ifstream fin("input.txt");
+	ofstream fout("output.txt");
 
 
 
