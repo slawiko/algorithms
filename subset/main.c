32d62e6e6e0c916b1ecb9e8125892d3818f7d742
slawiko <shchaurouski.slava@gmail.com>
2017-12-05 18:47:54 +0300

    subset

--- subset/main.c
+++ subset/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 struct Index {
-    long v;
+    double v;
     int i;
 };
 
@@ -10,55 +10,84 @@ int cmpfunc (const void * a, const void * b) {
     struct Index *i1 = (struct Index*)a;
     struct Index *i2 = (struct Index*)b;
     if (i1->v > i2->v) {
-        return 1;
+        return -1;
     }
     if (i1->v < i2->v) {
-        return -1;
+        return 1;
     }
     return 0;
 }
 
-long* find_subset(const long* a, const long* b, int n, int k) {
-    int t = n * 500000;
-    struct Index* sums = (struct Index*)malloc(k * sizeof(struct Index));
+int* find_subset(const int* a, const int* b, int n, int k, int b_max) {
+    double l = 0, m = n * 500000, r = n * 1000000;
+    struct Index* sums = (struct Index*)malloc(n * sizeof(struct Index));
 
-    for (int i = 0; i < n; ++i) {
-        sums[i] = (struct Index){a[i] - t * b[i], i};
+    double set_sum = 0;
+
+    while ((r - l) > 1. / (b_max * k)) {
+        set_sum = 0;
+        for (int i = 0; i < n; ++i) {
+            sums[i] = (struct Index){a[i] - m * b[i], i};
+        }
+
+        qsort(sums, n, sizeof(struct Index), cmpfunc);
+
+        for (int i = 0; i < k; ++i) {
+            set_sum += sums[i].v;
+        }
+        if (set_sum > m) {
+            l = m;
+        } else if (set_sum < m) {
+            r = m;
+        } else {
+            break;
+        }
+        m = l + (r - l) / 2.;
     }
 
-    qsort(sums, n, sizeof(struct Index), cmpfunc);
+    int* result = (int*)malloc(k * sizeof(int));
+    for (int i = 0; i < k; ++i) {
+        result[i] = sums[i].i + 1;
+    }
 
-    return sums;
+    free(sums);
+
+    return result;
 }
 
 int main() {
     int T;
     int n, k;
-    long *a, *b;
-    long *result;
+    int *a, *b;
+    int *result;
 
     scanf("%d", &T);
 
     for (int i = 0; i < T; ++i) {
         scanf("%d %d", &n, &k);
 
-        a = (long*)malloc(n * sizeof(long));
+        a = (int*)malloc(n * sizeof(int));
         for (int j = 0; j < n; ++j) {
-            scanf("%ld", &a[j]);
+            scanf("%d", &a[j]);
         }
 
-        b = (long*)malloc(n * sizeof(long));
+        int b_max = 0;
+        b = (int*)malloc(n * sizeof(int));
         for (int j = 0; j < n; ++j) {
-            scanf("%ld", &b[j]);
+            scanf("%d", &b[j]);
+            if (b[j] > b_max) b_max = b[j];
         }
 
-        result = find_subset(a, b, n, k);
+        result = find_subset(a, b, n, k, b_max);
         for (int j = 0; j < k; ++j) {
-            printf("%ld ", result[j]);
+            printf("%d ", result[j]);
         }
         printf("\n");
-    }
 
+        free(result);
+        free(a);
+        free(b);
+    }
 
 
     return 0;
