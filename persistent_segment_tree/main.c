de9cfc224a365488a47ddddf5aab5cc01cb6309d
slawiko <shchaurouski.slava@gmail.com>
2017-12-13 23:10:30 +0300

    persistent_segment_tree implementation

--- persistent_segment_tree/main.c
+++ persistent_segment_tree/main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
 
 struct Vertex {
-    int from;
-    int to;
     struct Vertex* l;
     struct Vertex* r;
     int min;
@@ -16,163 +13,67 @@ int cmp (const void * a, const void * b) {
     return (*(int*)a - *(int*)b);
 }
 
-void calc_add_info(struct Vertex* v) {
+void init(struct Vertex* v, int* a, int n, int i_from,  int min, int max) {
     if (v == NULL) {
         return;
     }
-    if (v->value == -1) {
-        v->odd_sum = v->l->odd_sum + v->r->odd_sum;
-        v->odd_cnt = v->l->odd_cnt + v->r->odd_cnt;
-        v->even_sum = v->l->even_sum + v->r->even_sum;
-        v->even_cnt = v->l->even_cnt + v->r->even_cnt;
-    } else {
-        bool is_even = v->value % 2 == 0;
-        v->odd_sum = is_even ? 0 : v->value;
-        v->even_sum = is_even ? v->value : 0;
-        v->odd_cnt = is_even ? 0 : 1;
-        v->even_cnt = is_even ? 1 : 0;
-    }
-    if (v->incr_cnt > 0) {
-        v->odd_sum += v->odd_cnt * v->incr_cnt;
-        v->even_sum += v->even_cnt * v->incr_cnt;
-
-        if (v->incr_cnt % 2 == 1) {
-            v->odd_sum = v->odd_sum ^ v->even_sum;
-            v->even_sum = v->odd_sum ^ v->even_sum;
-            v->odd_sum = v->odd_sum ^ v->even_sum;
-
-            v->odd_cnt = v->odd_cnt ^ v->even_cnt;
-            v->even_cnt = v->odd_cnt ^ v->even_cnt;
-            v->odd_cnt = v->odd_cnt ^ v->even_cnt;
-        }
-    }
-}
-
-void init(struct Vertex* v, int* a, int n, int i_from,  int from, int to) {
-    if (v == NULL) {
-        return;
-    }
-    v->from = from;
-    v->to = to;
     if (n > 1) {
         int n_l = n / 2 + (n % 2 == 0 ? 0 : 1);
         int n_r = n / 2;
 
         v->l = (struct Vertex*)malloc(sizeof(struct Vertex));
-        init(v->l, a, n_l, i_from, from, a[i_from + n_l - 1]);
+        init(v->l, a, n_l, i_from, min, a[i_from + n_l - 1]);
         v->r = (struct Vertex*)malloc(sizeof(struct Vertex));
-        init(v->r, a, n_r, i_from + n_l, a[i_from + n_l], to);
+        init(v->r, a, n_r, i_from + n_l, a[i_from + n_l], max);
     } else {
         v->l = NULL;
         v->r = NULL;
     }
-    v->min = from;
-    v->max = to;
-    v->cnt = n;
+    v->min = min;
+    v->max = max;
+    v->cnt = 0;
 }
 
-void incr_incr(struct Vertex* v, int incr) {
-    if (v->incr_cnt == -1) {
-        v->value += incr;
-    } else {
-        v->incr_cnt += incr;
-    }
-}
-
-void check_if_incr(struct Vertex* v) {
-    if (v == NULL) {
-        return;
-    }
-    if (v->incr_cnt > 0 && v->value == -1) {
-        incr_incr(v->l, v->incr_cnt);
-        calc_add_info(v->l);
-        incr_incr(v->r, v->incr_cnt);
-        calc_add_info(v->r);
-        v->incr_cnt = 0;
-    }
-}
-//
-//void inc(struct Vertex* v, int l, int r, int c) {
-//    if (v == NULL) {
-//        return;
-//    }
-//    if (v->from > l) l = v->from;
-//    if (v->to < r) r = v->to;
-//    if (l > r) return;
-//
-//    check_if_incr(v);
-//
-//    if (v->from == l && v->to == r) {
-//        if (v->incr_cnt == -1) {
-//            v->value += c;
-//        } else {
-//            v->incr_cnt += c;
-//        }
-//    } else {
-//        inc(v->l, l, r, c + v->incr_cnt);
-//        inc(v->r, l, r, c + v->incr_cnt);
-//        v->incr_cnt = 0;
-//    }
-//    calc_add_info(v);
-//}
-//
 struct Vertex* set(struct Vertex* v, int value) {
     if (v == NULL) {
-        return;
+        return NULL;
     }
-    if (v->from > pos || pos > v->to) {
-        return;
-    }
-
-    check_if_incr(v);
-
-    if (v->from == pos && v->to == pos) {
-        v->value = value;
+    struct Vertex* temp = (struct Vertex*)malloc(sizeof(struct Vertex));
+    if (v->min == value && v->max == value) {
+        temp->l = NULL;
+        temp->r = NULL;
+        temp->min = value;
+        temp->max = value;
+        temp->cnt = v->cnt + 1;
+    } else if (v->l->min <= value && value <= v->l->max) {
+        temp->l = set(v->l, value);
+        temp->r = v->r;
+        temp->min = v->min;
+        temp->max = v->max;
+        temp->cnt = temp->l->cnt + v->r->cnt;
     } else {
-        set(v->l, pos, value);
-        set(v->r, pos, value);
+        temp->l = v->l;
+        temp->r = set(v->r, value);
+        temp->min = v->min;
+        temp->max = v->max;
+        temp->cnt = v->l->cnt + temp->r->cnt;
     }
-    calc_add_info(v);
-}
-//
-//long long getSumEven(struct Vertex* v, int l, int r) {
-//    if (v == NULL) {
-//        return 0;
-//    }
-//    if (v->from > l) l = v->from;
-//    if (v->to < r) r = v->to;
-//    if (l > r) return 0;
-//
-//    if (v->from == l && v->to == r) {
-//        return v->even_sum;
-//    }
-//    check_if_incr(v);
-//    return getSumEven(v->l, l, r) + getSumEven(v->r, l, r);
-//}
-//
-//long long getSumOdd(struct Vertex* v, int l, int r) {
-//    if (v == NULL) {
-//        return 0;
-//    }
-//    if (v->from > l) l = v->from;
-//    if (v->to < r) r = v->to;
-//    if (l > r) return 0;
-//
-//    if (v->from == l && v->to == r) {
-//        return v->odd_sum;
-//    }
-//    check_if_incr(v);
-//    return getSumOdd(v->l, l, r) + getSumOdd(v->r, l, r);
-//}
 
-void clean(struct Vertex* v) {
-    if (v->l != NULL) {
-        clean(v->l);
+    return temp;
+}
+
+int getCnt(struct Vertex* v, int x, int y) {
+    if (v == NULL) {
+        return 0;
     }
-    if (v->r != NULL) {
-        clean(v->r);
+    if (v->min > x) x = v->min;
+    if (v->max < y) y = v->max;
+    if (x > y) return 0;
+
+    if (v->min == x && v->max == y) {
+        return v->cnt;
     }
-    free(v);
+    return getCnt(v->l, x, y) + getCnt(v->r, x, y);
 }
 
 int main() {
@@ -209,14 +110,16 @@ int main() {
         }
     }
 
-    int l, r, x, y;
+    int l, r, x, y, cnt;
     for (int i = 0; i < Q; ++i) {
         scanf("%d %d %d %d", &l, &r, &x, &y);
+        cnt = getCnt(roots[r], x, y) - getCnt(roots[l - 1], x, y);
+        printf("%d\n", cnt);
     }
 
     free(a);
     for (int i = 0; i < N + 1; ++i) {
-        clean(roots[i]);
+        free(roots[i]);
     }
     return 0;
 }
